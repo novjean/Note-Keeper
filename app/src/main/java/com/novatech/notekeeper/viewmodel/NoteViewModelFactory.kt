d909bff648541842f45544998cb5e1197fb81326
Novjean J Kannathara <novjeanj@gmail.com>
2024-05-14 22:14:57 +0530

    view model factory defined

---
+++ app/src/main/java/com/novatech/notekeeper/viewmodel/NoteViewModelFactory.kt
@@ -0,0 +1,15 @@
+package com.novatech.notekeeper.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.novatech.notekeeper.repository.NoteRepository
+
+class NoteViewModelFactory(val app : Application,
+    private val noteRepository: NoteRepository
+    ) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return NoteViewModel(app, noteRepository) as T
+    }
+}
