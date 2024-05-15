1bae1aed573610b1075b20ce6dc7971626396674
Novjean J Kannathara <novjeanj@gmail.com>
2024-05-15 13:20:21 +0530

    main activity wiring and theming

--- app/src/main/java/com/novatech/notekeeper/MainActivity.kt
+++ app/src/main/java/com/novatech/notekeeper/MainActivity.kt
@@ -5,19 +5,38 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.novatech.notekeeper.database.NoteDatabase
+import com.novatech.notekeeper.databinding.ActivityMainBinding
+import com.novatech.notekeeper.repository.NoteRepository
 import com.novatech.notekeeper.viewmodel.NoteViewModel
+import com.novatech.notekeeper.viewmodel.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     lateinit var noteViewModel: NoteViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+//        enableEdgeToEdge()
+
+        binding  = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupViewModel()
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun setupViewModel() {
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application, noteRepository)
+
+        noteViewModel = ViewModelProvider(this, viewModelProviderFactory)
+            .get(NoteViewModel::class.java)
     }
 }
